f46ded76e442e325890613b4dcb8cbe5dfc94188
Pavel Mayboroda <Successbloger1@yandex.ua>
2016-03-22 14:34:42 +0300

    Добавлено решение задания 5

---
+++ dz5/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Урок 5</title>
+    </head>
+    <body>
+        <h2>Урок 5</h2>
+        <ul>
+            <li><a href="dz5_1.php">Метод GET</a></li>
+            <li><a href="dz5_2.php">Метод POST</a></li>
+        </ul>
+    </body>
+</html>
