ea6b8992a700f82e73ae2025dd18ce56e52eb801
Pavel Mayboroda <Successbloger1@yandex.ua>
2016-04-07 10:00:50 +0300

    Damper added

---
+++ sxd/backup/sj_ads_db.job.php
@@ -0,0 +1,20 @@
+<?php
+$JOB = array (
+  'type' => 'backup',
+  'db' => 'ads_db',
+  'charset' => 'utf8',
+  'zip' => '7',
+  'comment' => '',
+  'del_time' => '30',
+  'del_count' => '',
+  'obj' => 
+  array (
+    'TA' => 
+    array (
+      0 => '*',
+    ),
+  ),
+  'job' => 'ads_db',
+  'title' => '',
+);
+?>
---
+++ sxd/ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  '7c7d184817d2d8a5708f457870623b53' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'ru',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'admin',
+      'my_pass' => '123',
+      'my_comp' => 0,
+      'my_db' => 'ads_db',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1459940496,
+    'lng' => 'ru',
+  ),
+);
+?>
