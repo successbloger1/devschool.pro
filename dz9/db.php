70ef8315694095e65ae1d2f82620c400b7bed929
Pavel Mayboroda <Successbloger1@yandex.ua>
2016-05-25 10:43:12 +0300

    -

---
+++ dz9/db.php
@@ -0,0 +1,5 @@
+<?php
+$server_name = 'localhost';
+$user_name = 'root';
+$password = '123';
+$database = 'qwqw';
---
+++ dz9/smarty/templates_c/%%F4^F45^F45DF2C8%%install_success.tpl.php
@@ -0,0 +1,11 @@
+<?php /* Smarty version 2.6.25-dev, created on 2016-04-07 15:22:02
+         compiled from install_success.tpl */ ?>
+<html>
+    <head>
+       
+    </head>
+    <body>
+        <center><span id="success"><?php echo $this->_tpl_vars['success']; ?>
+</span></center>
+    </body>
+</html>
