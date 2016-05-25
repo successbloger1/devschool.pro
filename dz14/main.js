9b5100e461895f1984c5dfcc4e02143c7570b899
Pavel Mayboroda <Successbloger1@yandex.ua>
2016-05-25 10:40:57 +0300

    14 задание

---
+++ dz14/main.js
@@ -0,0 +1,7 @@
+ window.onload = function() {
+     document.getElementById("testButton").onclick = function() {
+         document.body.style.backgroundColor = "yellow"; //fon
+         document.body.style.color = "red"; //text
+     };
+
+ };
